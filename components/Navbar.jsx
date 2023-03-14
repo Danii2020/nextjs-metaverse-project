2e95d0d9d7de1b804b7a7138eec4edeaf20c28a5
danii2020 <danielmauricioerazoespinoza@gmail.com>
2023-03-14 17:12:34 -0500

    First commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,42 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import styles from '../styles';
+import { navVariants } from '../utils/motion';
+import Image from 'next/image';
+
+const Navbar = () => (
+  <motion.nav
+    variants={navVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-8 relative`}
+  >
+    <div className='absolute w-[50%] inset-0
+    gradient-01'/>
+    <div className={`${styles.innerWidth} mx-auto
+    flex justify-between gap-8`}>
+        <Image
+        className='object-contain'
+        width={"24"}
+        height={"24"}
+        src="/search.svg" alt="search"
+        />
+        <h2
+        className='font-extrabold text-[24px]
+        leading-[30px] text-white'
+        >
+            METAVERSUS
+        </h2>
+        <Image
+        className="object-contain"
+        width={"24"}
+        height={"24"}
+        src="/menu.svg"
+        alt="menu"
+        />
+    </div>
+  </motion.nav>
+);
+
+export default Navbar;
